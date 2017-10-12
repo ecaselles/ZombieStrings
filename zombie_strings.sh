039dfaa68acfb85b23cabcaf575c4dabd9b8414a
Edu Caselles <edu@casell.es>
2017-10-12 15:32:16 +0100

    Prefers spaces over tabs for indentation

--- zombie_strings.sh
+++ zombie_strings.sh
@@ -2,22 +2,22 @@
 
 rootFolder=.
 if [[ $1 ]]; then
-	rootFolder=$1
+  rootFolder=$1
 fi
 
 allLocalisableFiles=$(find $rootFolder -type f -name "*.strings");
 for localisableFile in $allLocalisableFiles; do
-	echo -e "\n🔎  Inspecting:" $localisableFile
+  echo -e "\n🔎  Inspecting:" $localisableFile
 
-	while read p; do
-		IFS=" = ";
-		string=($p);
-		key=${string[0]}
-		if [[ ${#string[@]} -gt 1 ]] && [[ $key == \"* ]]; then
-			if ! grep -rsq --include=\*.{swift,m,h} $key $rootFolder; then
-				echo "⚠️ " $key "is not used 💀"
-			fi
-		fi
-		unset IFS;
-	done < $localisableFile
+  while read p; do
+    IFS=" = ";
+    string=($p);
+    key=${string[0]}
+    if [[ ${#string[@]} -gt 1 ]] && [[ $key == \"* ]]; then
+      if ! grep -rsq --include=\*.{swift,m,h} $key $rootFolder; then
+        echo "⚠️ " $key "is not used 💀"
+      fi
+    fi
+    unset IFS;
+  done < $localisableFile
 done
